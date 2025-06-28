4a18cac4b4ed1febea37f50b8d1666006e8a35b7
Anshul Kumar Sharma <136547509+Anshulsharma04@users.noreply.github.com>
2025-06-29 00:38:39 +0530

    Add .next/static/chunks/src_app_about_page_tsx_17b3d179._.js 1751137719282

---
+++ .next/static/chunks/src_app_about_page_tsx_17b3d179._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c8fd9c4d._.js",
+  "static/chunks/src_79d3e633._.js"
+],
+    source: "dynamic"
+});
