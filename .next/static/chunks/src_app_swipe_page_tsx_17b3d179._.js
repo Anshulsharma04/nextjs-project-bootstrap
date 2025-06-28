0ee299bbd1155882887640c74dc3387248ed3b67
Anshul Kumar Sharma <136547509+Anshulsharma04@users.noreply.github.com>
2025-06-29 00:38:26 +0530

    Add .next/static/chunks/src_app_swipe_page_tsx_17b3d179._.js 1751137705383

---
+++ .next/static/chunks/src_app_swipe_page_tsx_17b3d179._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_fb4e5055._.js",
+  "static/chunks/src_c4686424._.js"
+],
+    source: "dynamic"
+});
