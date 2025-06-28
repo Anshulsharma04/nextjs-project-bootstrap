a5f2fc7a89945b6839ca5b7cb4af666172a7560f
Anshul Kumar Sharma <136547509+Anshulsharma04@users.noreply.github.com>
2025-06-29 00:38:07 +0530

    Add .next/static/chunks/src_app_chat_page_tsx_17b3d179._.js 1751137687377

---
+++ .next/static/chunks/src_app_chat_page_tsx_17b3d179._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6b999aeb._.js",
+  "static/chunks/src_4c995669._.js"
+],
+    source: "dynamic"
+});
